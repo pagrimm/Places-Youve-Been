1971afaf9ad3d97ae4dd693ea677cfee81d9f1f0
Taylor Phillips <taylorphillips133@gmail.com>
2020-06-15 10:55:48 -0700

    add a contructor for place and for our passport and prototype functions for adding a place and assigning the corresponding Id
    Co-Authored-By: Peter Grimm <pagrimm@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,40 @@
+function Place(location, landmark, dateVisited, notes)  {
+  this.location = location,
+  this.landmark = landmark,
+  this.dateVisited = dateVisited,
+  this.notes = notes
+}
+
+function Passport() {
+  this.places = [],
+  this.currentId = 0
+}
+Passport.prototype.addPlace = function(place) {
+  place.id = this.assigndId();
+  this.places.push(place);
+}
+Passport.prototype.assignId = function()  {
+  this.currentId += 1;
+  return this.currentId;
+}
+
+//1. Display properties when a user clicks the place's name.
+//2. Add specs to README.md
+$(document).ready(function () {
+  attachContactListeners();
+  $("form#new-contact").submit(function (event) {
+    event.preventDefault();
+    const newPlace = $("input#new-first-name").val();
+    const newPlaceLandmark = $("input#new-last-name").val();
+    const inputtedPhoneNumber = $("input#new-phone-number").val();
+
+    // The next three lines are new:
+    $("input#new-first-name").val("");
+    $("input#new-last-name").val("");
+    $("input#new-phone-number").val("");
+
+    let newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber);
+    addressBook.addContact(newContact);
+    displayContactDetails(addressBook);
+  })
+})
